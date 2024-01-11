da4faa8e02ace785fef2db5bcc2c92d11f2c7b9f
PeterMaingi-dev <petermaingi435@gmail.com>
2024-01-11 16:01:07 +0300

    Update config.js
    Create default Mongo cloud database for the project

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 8080;
 
 export const mongoDBURL = 
-'mongodb+srv://root:mern123@book-store-mern.sip6oek.mongodb.net/?retryWrites=true&w=majority'
+'mongodb+srv://root:<password>@book-store-mern.sip6oek.mongodb.net/?retryWrites=true&w=majority'
 
     //This database is not available on public.
     //please create free database on https://cloud.mongodb.com
